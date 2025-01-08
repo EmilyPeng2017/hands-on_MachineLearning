f254e4da4e32e94cdc32ed52de6278fef8ec5758
Emily Peng <emily.peng@kuleuven.be>
2025-01-08 15:50:10 +0100

    ch2

--- ch2/example2.py
+++ ch2/example2.py
@@ -2,6 +2,10 @@ from pathlib import Path
 import pandas as pd
 import tarfile
 import urllib.request
+import matplotlib.pyplot as plt
+import numpy as np
+from zlib import crc32
+
 
 def load_housing_data():
     tarball_path = Path("Datasets/housing.tgz")
@@ -13,4 +17,25 @@ def load_housing_data():
             housing_tarball.extractall(path="datasets")
     return pd.read_csv(Path("datasets/housing/housing.csv"))
 
+def shuffle_and_split_data(data, test_ratio):
+  shuffled_indices = np.random.permutation(len(data))
+  test_set_size = int(len(data) * test_ratio)
+  test_indices = shuffled_indices[:test_set_size]
+  train_indices = shuffled_indices[test_set_size:]
+  return data.iloc[train_indices], data.iloc[test_indices]
+
+def is_id_in_test_set(identifier, test_ratio):
+  return crc32(np.int64(identifier)) < test_ratio * 2**32
+
+def split_data_with_id_hash(data, test_ratio, id_column):
+  ids = data[id_column]
+  in_test_set = ids.apply(lambda id_: is_id_in_test_set(id_, test_ratio))
+  return data.loc[~in_test_set], data.loc[in_test_set]
+
 housing = load_housing_data()
+housing.head()
+housing.info()
+
+
+# housing.hist(bins=50, figsize=(12, 8))
+# plt.show()
